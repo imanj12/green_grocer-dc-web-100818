7a9d64c4e7f9279bb3d906343138043f0a307717
Iman Jawad <imanj12@gmail.com>
2018-10-02 23:07:53 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -12,7 +12,9 @@ end
 def apply_coupons(cart, coupons)
   coupons.each do |coupon|
     coupon_name = coupon[:item]
-    if cart[coupon_name] && cart[coupon_name][:count] 
+    if cart[coupon_name] && cart[coupon_name][:count] >= coupon[:num]
+      
+    end
   end
 end
 
