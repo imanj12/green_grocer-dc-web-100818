e6466a7efd90b065c42dd67263af657afbca25a7
Iman Jawad <imanj12@gmail.com>
2018-10-02 23:28:51 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -13,7 +13,7 @@ def apply_coupons(cart, coupons)
   coupons.each do |coupon|
     coupon_name = coupon[:item]
     if defined? cart[coupon_name] && cart[coupon_name][:count] >= coupon[:num]
-      if cart["#{coupon_name} W/COUPON"]
+      if defined? cart["#{coupon_name} W/COUPON"]
         cart["#{coupon_name} W/COUPON"][:count] += 1
       else
         cart["#{coupon_name} W/COUPON"] = {:count => 1, :price => coupon[:cost]}
